0ca8fcde8400912f200a74e3a0c0696981ffae0d
Szabolcs Hodossy <hodossy.szabolcs@gmail.com>
2017-09-10 09:37:41 +0200

    Added components and services to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PlayerComponent } from './player/player.component';
+import { PlayerListComponent } from './player-list/player-list.component';
 import { DartsTableComponent } from './darts-table/darts-table.component';
+import { PlayerAddComponent } from './player-add/player-add.component';
+import { PlayerService } from './player/player.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlayerComponent,
-    DartsTableComponent
+    PlayerListComponent,
+    DartsTableComponent,
+    PlayerAddComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ PlayerService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

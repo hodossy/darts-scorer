a1b2821e7561afb18590d904c0e86e658db6a626
Szabolcs Hodossy <hodossy.szabolcs@gmail.com>
2017-10-01 16:16:12 +0200

    Auto-generated commit

--- src/app/core/throw.model.ts
+++
@@ -1,7 +0,0 @@
-export class Throw {
-  constructor(public sector: number, public multiplier: number) { }
-
-  get value() {
-    return this.sector * this.multiplier;
-  }
-}
--- src/app/games/x01-game/x01-score/x01-score.model.ts
+++
@@ -1,5 +0,0 @@
-export class X01Score {
-  public current = 0;
-  public legs = 0;
-  public sets = 0;
-}

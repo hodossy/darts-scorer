c62346a9ab225954189002ceed3016836d952669
Hodossy Szabolcs <szabolcs.hodossy@nokia.com>
2018-01-27 11:10:42 +0100

    darts table with svg

--- src/app/darts-table/darts-table.component.ts
+++ src/app/darts-table/darts-table.component.ts
@@ -10,26 +10,7 @@ import { Throw } from '../core/throw.model';
 export class DartsTableComponent {
   @Output() throw: EventEmitter<Throw> = new EventEmitter<Throw>();
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  onClick(event) {
-    let target = event.target || event.srcElement || event.currentTarget;
-    let id = target.attributes.id.nodeValue;
-    let multiplier = 0;
-    switch(id.slice(0, 1)) {
-      case "s":
-        multiplier = 1;
-        break;
-      case "d":
-        multiplier = 2;
-        break;
-      case "t":
-        multiplier = 3;
-        break;
-    }
-    this.throw.emit(new Throw(+id.slice(1), multiplier));
+  onClick(sector, multiplier) {
+    this.throw.emit(new Throw(+sector, +multiplier));
   }
 }

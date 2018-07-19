326aad1a51ade2323154606bb3240782f5bcfb79
Szabolcs Hodossy <hodossy.szabolcs@gmail.com>
2018-07-19 20:10:19 +0200

    Added warning when a game is started without players

--- src/app/games/game-base/game-base.component.ts
+++ src/app/games/game-base/game-base.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit, TemplateRef } from '@angular/core';
+import { Component, Input, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { MatInputModule } from '@angular/material';
+import { MatTabGroup } from '@angular/material/tabs';
 
 import { SharedModule } from '../../shared/shared.module';
 import { GameBaseService } from './game-base.service'
@@ -13,7 +14,24 @@ export class GameBaseComponent implements OnInit {
   @Input() settingsTemplate: TemplateRef<any>;
   @Input() scoreTemplate: TemplateRef<any>;
 
+  @ViewChild(MatTabGroup) tabs: MatTabGroup;
+
+  startClicked: boolean;
+
   constructor(private game: GameBaseService) { }
+
+  get noPlayer() {
+    return this.startClicked && this.game.players.length == 0;
+  }
+
+  onStart() {
+    if(this.game.players.length == 0) {
+      this.tabs.selectedIndex = 0;
+      this.startClicked = true;
+    } else {
+      this.game.onStart();
+    }
+  }
 
   ngOnInit() {}
 }

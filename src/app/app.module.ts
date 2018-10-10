109906412f756dc3ad7a64d01ba64811af51ab1d
Hodossy Szabolcs <szabolcs.hodossy@nokia.com>
2018-10-10 12:02:53 +0200

    Correct Gapi service and compile errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 /* Root component*/
 import { AppComponent } from './app.component';
 /* Core module: Angular-Material */
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { GoogleService } from './social/google.service';
 
 import { DartsTableComponent } from './darts-table/darts-table.component';
 import { PlayerExtendableListComponent } from './player-extendable-list/player-extendable-list.component';
@@ -17,6 +18,7 @@ import { X01SettingsComponent } from './games/x01-game/x01-settings/x01-settings
 import { CricketGameComponent } from './games/cricket-game/cricket-game.component';
 import { CricketScoreComponent } from './games/cricket-game/cricket-score/cricket-score.component';
 import { PageNotFoundComponent } from './not-found/not-found.component';
+import { SigninComponent } from './signin/signin.component';
 
 export const appRoutes: Routes = [
   { path: 'games/x01', component: X01GameComponent },
@@ -24,6 +26,10 @@ export const appRoutes: Routes = [
   { path: '',   redirectTo: '/games/x01', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ]
+
+export function initGapi(gapiSession: GoogleService) {
+  return () => gapiSession.initClient();
+}
 
 @NgModule({
   declarations: [
@@ -44,6 +50,9 @@ export const appRoutes: Routes = [
     CoreModule,
     SharedModule,
     RouterModule.forRoot(appRoutes)
+  ],
+  providers: [
+    { provide: APP_INITIALIZER, useFactory: initGapi, deps: [GoogleService], multi: true }
   ],
   bootstrap: [AppComponent]
 })
